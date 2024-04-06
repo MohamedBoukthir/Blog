958c748092f36f600c4b027bfc5c841376e2904e
MohamedBoukthir <medboukthir7@gmail.com>
2024-04-06 03:24:45 +0100

    Finalize repositories creation for User, Post, Category and Comment

---
+++ backend/src/main/java/com/mohamed/Repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.mohamed.Repositories;
+
+import com.mohamed.Entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long > {
+}
